db3a7d561b737eb1ac5d3b51eeb1643044d9fa2e
Ludovic Dillieux <ludovic.dillieux@gmail.com>
2025-09-14 11:48:41 +0200

    Modification du manifest.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';  
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
@@ -20,6 +20,10 @@ export default defineConfig({
         VitePWA({
             registerType: 'autoUpdate',
             includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+            // Génère le manifest PWA à la racine public/
+            manifestFilename: 'manifest.json',
+            publicDir: false, // Important
+            outDir: '../public', // Génère dans public/ directement
             manifest: {
                 name: 'BeeNote - Gestion de Ruchers',
                 short_name: 'BeeNote',
